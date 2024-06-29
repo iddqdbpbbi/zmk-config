be3683f842dadfda385b85dc0bf2f988d8561a9b
Artem Sergeev <aosergeev@yandex.ru>
2024-06-29 18:15:56 +0300

    add shell scripta

--- copy.sh
+++ copy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-move 
+mv ~/Downloads/firmware.zip  ./artifacts
+unzip -d ./artifacts/ ./artifacts/firmware.zip  
+rm ~/project/keyboards/qdkbd/zmk-config/artifacts/firmware.zip
+udiskie-mount -a
+if [ -f ./artifacts/qdkbd_left-nice_nano_v2-zmk.uf2 ]; then
+    mv ./artifacts/qdkbd_left-nice_nano_v2-zmk.uf2 /media/qd/NICENANO
+    echo Copy left
+elif [ -f ./artifacts/qdkbd_right-nice_nano_v2-zmk.uf2 ]; then
+    mv ./artifacts/qdkbd_right-nice_nano_v2-zmk.uf2 /media/qd/NICENANO
+    echo Copy right    
+else
+    echo Nothing to copy
+fi 
